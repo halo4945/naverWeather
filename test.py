6b73b2d78200d9422f827b95321ddfdfc87d98ee
halo4945 <44014842+halo4945@users.noreply.github.com>
2021-10-18 22:47:41 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
-from naverWeather import *
-l = naverWeather.map_cityNum.keys()
-print(naverWeather().getWeather())
-for s in l :
+from naverWeather import * #naverWeather 모듈 import
+l = naverWeather.map_cityNum.keys() # l에 key값들 리스트 저장
+print(naverWeather().getWeather()) #?
+for s in l :#iterator
     print(naverWeather(s).getWeather())
